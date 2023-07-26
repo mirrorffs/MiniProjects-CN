6bb2d3631c96000911fb6b857a3fadde6935b42a
Avi Gupta <86228752+mirrorffs@users.noreply.github.com>
2023-07-26 06:19:37 +0530

    final-react-app

---
+++ CoursesSite/src/pages/misc/Page404/Page404.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import style from "./Page404.module.css";
+
+import { useLocation, useNavigate } from "react-router-dom";
+function Page404() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(()=>{
+    setTimeout(()=>(navigate('/')),3000)
+  })
+
+  return (
+    <div className={style.errorPage}>
+      <h1>Oops! Something went Wrong.</h1>
+      <h2>Page Not Found</h2>
+      <h4>
+        The page <code>{location.pathname}</code> does not exist.
+      </h4>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Go to Home
+      </button>
+    </div>
+  );
+}
+
+export default Page404;
