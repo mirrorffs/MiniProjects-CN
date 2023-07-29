c4e2bcbe9312798673a9376991b066331d7e7d7c
Avi Gupta <86228752+mirrorffs@users.noreply.github.com>
2023-07-30 02:10:08 +0530

    incorrect

--- counter:timer/src/redux/actions/counterActions.js
+++
@@ -1,8 +0,0 @@
-export const DECREMENT_COUNTER = "Start COUNTER";
-export const RESET_COUNTER = "Reset COUNTER";
-export const INCREMENT_COUNTER = "Increment COUNTER";
-
-// action creators
-export const decrementCOUNTER = () => ({ type: DECREMENT_COUNTER });
-export const resetCOUNTER = () => ({ type: RESET_COUNTER });
-export const incrementCOUNTER = () => ({ type: INCREMENT_COUNTER });
