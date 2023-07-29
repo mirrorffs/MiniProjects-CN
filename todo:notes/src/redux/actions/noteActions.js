5d691f18053ef359b56f501e1e8bc9b1a14ab0ab
Avi Gupta <86228752+mirrorffs@users.noreply.github.com>
2023-07-30 02:09:30 +0530

    incorrect

--- todo:notes/src/redux/actions/noteActions.js
+++
@@ -1,5 +0,0 @@
-export const ADD_NOTE='Add Note'
-export const DELETE_NOTE='Delete Note'
-
-export const addNote = (text)=>({text, type:ADD_NOTE});
-export const deleteNote = (index)=>({index, type: DELETE_NOTE});
